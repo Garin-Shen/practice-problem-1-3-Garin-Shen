41ecb6e8a4c970e506ca14717ff1a8626a0897ed
Garin-Shen-ICD2O1-01 <garin.shen@student.tdsb.on.ca>
2024-09-18 03:11:12 +0800

    Update PP1_3.py

--- PP1_3.py
+++ PP1_3.py
@@ -1,16 +1,28 @@
 
 
 def q1():
-  #Write Assignment code here
-  #Remember your code must be indented once
+#a
+for i in range(6):
+  if i % 2 = 0:
+    print("* * * * * *")
+  else:
+    print(" * * * * * *")
+#b
+for i in range(5)
+  if i % 4 = 1
+    print("* * * * *")
+  else:
+    print("*       *")
+#c
+print("* * * * * *"/n" *       *"/n"  *     *"/n"   *   *"/n"    * *"/n"     *"/n)
 
 def q2():
-  #Write Assignment code here
-  
-def q3():
-  #Write Assignment code here
-  
 
+print("Hello "/n"World")
+
+def q3():
+
+print('"Man"\n"Board"\n"Man Overboard"')
 
 #Do not alter anything after this line
 q1()
